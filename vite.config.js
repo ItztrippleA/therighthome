e39e846e44c91df7b344f8c242668a7bc2f4bac3
itztrippleA <abdullahiadam.aa@gmail.com>
2024-07-11 01:34:53 +0100

    vite leaflet import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// import vitePluginCssImport from "vite-plugin-css-import";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      external: ["leaflet/dist/leaflet.css"],
+    },
+  },
+});
