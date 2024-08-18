f5fe7bd908ce21ee2b755b55815d84bbb3640d3b
itztrippleA <abdullahiadam.aa@gmail.com>
2024-08-18 02:17:53 +0100

    edit

--- src/utils/accordion.jsx
+++ src/utils/accordion.jsx
@@ -3,19 +3,19 @@ import { MdCancel, MdAnalytics } from "react-icons/md";
 const data = [
   {
     icon: <HiShieldCheck />,
-    heading: "Best interest rates on the market",
+    heading: "Distress Sale",
     detail:
       "Need to sell quickly? We're here to help you turn your property into cash fast.Let us take the stress out of vour situation",
   },
   {
     icon: <MdCancel />,
-    heading: "Prevent unstable prices",
+    heading: "Shared Appartment",
     detail:
       "Share a space, share the cost and double the fun --Find your perfect roomate today",
   },
   {
     icon: <MdAnalytics />,
-    heading: "Best price on the market",
+    heading: "Book Hotel",
     detail:
       "Turn every trip into an unforgettable experience!... Book your stay today",
   },
