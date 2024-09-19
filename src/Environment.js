e677b710e6f7e06c103151c143a0b1206efaa75b
itztrippleA <abdullahiadam.aa@gmail.com>
2024-09-19 03:42:53 +0100

    update

--- src/Environment.js
+++ src/Environment.js
@@ -5,8 +5,8 @@ export const IOS_CLIENTID =
   "462629199995-92bl9qi1jir7uf5si8cocit18gn3kivm.apps.googleusercontent.com";
 export const GOOGLE_API_KEY = "AIzaSyBEAff023nqPe0jS--VQ69d996NXOHHt0g";
 
-// export const BASE_URL = "https://derighthomeapi-1.onrender.com";
-export const BASE_URL = "http://localhost:8800";
+export const BASE_URL = "https://derighthomeapi-1.onrender.com";
+// export const BASE_URL = "http://localhost:8800";
 export const PAYSTACK_KEY = "pk_live_0b48143fd6d4165471453033a4e87bd58ec7454b";
 // export const PAYSTACK_KEY = "pk_test_539dc81d1327ebb1034df4f3d510b88e59044eaa";
 
